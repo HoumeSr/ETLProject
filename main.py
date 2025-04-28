6c390420e788b8c1d18ebfd0aeddf166f04f8ea0
RussianPlay <ahmadullin32@mail.ru>
2025-04-29 01:53:23 +0300

    comm_4

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import clickhouse_connect
 import pandas as pd
 import config as config
 import postgresql_query as query
@@ -6,8 +5,6 @@ import clickhouse_query as ch_query
 from PostgreSQLDatabase import PostgreSQLDatabase
 from ClickHouseClient import ClickHouseClient
 from psycopg.errors import OperationalError
-from datetime import datetime
-import openpyxl
 
 
 def create_data(cur):
@@ -32,15 +29,15 @@ def insert_data(cur, lst_of_tuples):
     insert_into_all_tables(cur)
 
 
-# def prepare_data(df):
-#     """Преобразуем DataFrame в список кортежей с обработкой NULL"""
-#     my_list = []
-#     for _, row in df.iterrows():
-#         # Заменяем NaN/None на None (для PostgreSQL NULL)
-#         row = row.where(pd.notnull(row), None)
-#         # Конвертируем в кортеж
-#         my_list.append(tuple(row))
-#     return my_list
+def prepare_data(df):
+    """Преобразуем DataFrame в список кортежей с обработкой NULL"""
+    my_list = []
+    for _, row in df.iterrows():
+        # Заменяем NaN/None на None (для PostgreSQL NULL)
+        row = row.where(pd.notnull(row), None)
+        # Конвертируем в кортеж
+        my_list.append(tuple(row))
+    return my_list
 
 
 def init_postgreSQLDatabase(cur):
@@ -54,17 +51,8 @@ def init_postgreSQLDatabase(cur):
 
         # 3. Чтение и подготовка данных
         # Это нужно откомментировать
-        # df = pd.read_excel(config.file_path)
-        # data = prepare_data(df)
-
-        # Это я сделал только для себя. Считывает только первые 10 строчек из xls файла
-        data = []
-        book = openpyxl.load_workbook(filename=config.file_path, read_only=True, data_only=True)
-        first_sheet = book.worksheets[0]
-        rows_generator = first_sheet.values
-        header_row = next(rows_generator)
-        for index, row in zip(range(10), rows_generator):
-            data.append(row)
+        df = pd.read_excel(config.file_path)
+        data = prepare_data(df)
 
         # 4. Вставка данных
         insert_data(cur, data)
@@ -73,30 +61,29 @@ def init_postgreSQLDatabase(cur):
         print(f"Ошибка при выполнении команд: {e}")
 
 
-def connect_to_clickhouse(cur):
-    client = ClickHouseClient()
-
-    with client as client_cur:
-        # Пример выполнения запроса
-        # client.query(query.clickhouse_create_query)
-        lst = [
-        {'id': 1, 'name': 'Товар 3', 'price': 100.50, 'timestamp': '2023-05-15 14:30:22'},
-        {'id': 2, 'name': 'Товар 4', 'price': 200.00, 'timestamp': '2023-05-15 14:30:22'}]
-        row1 = ['Товар 1', 100.50, 1, datetime.now()]
-        row2 = ['Товар 1', 100.50, 1, datetime.now()]
-        data = [row1, row2]
-
-        client_cur.execute(ch_query.clickhouse_insert_query, data)
+def connect_to_clickhouse(client_cur, data):
+    # c.query(ch_query.drop_purchases)
+    # c.query(ch_query.drop_date_purchases)
+    # c.query(ch_query.drop_date_purchases_by_gender)
+    for i in range(len(data)):
+        client_cur.execute(ch_query.create_purchases, data[i])
+    client_cur.execute(ch_query.create_date_purchases)
+    client_cur.execute(ch_query.create_date_purchases_by_gender)
+    client_cur.execute(ch_query.insert_data_to_purchases)
+    client_cur.execute(ch_query.insert_data_to_date_purchases)
+    client_cur.execute(ch_query.insert_data_to_date_purchases_by_gender)
+    amount = client_cur.execute(ch_query.get_sum_all_time).first_row[0]
+    amount_gender_E = client_cur.execute(ch_query.get_sum_all_time_by_gender.format(gender="E")).first_row[0]
+    amount_gender_K = client_cur.execute(ch_query.get_sum_all_time_by_gender.format(gender="K")).first_row[0]
+    print(amount, amount_gender_E, amount_gender_K)
 
 
 if __name__ == "__main__":
     postgre_db = PostgreSQLDatabase()
     ch_client = ClickHouseClient()
-    # Это нужно откомментировать, когда таблица postgresql пустая
-    # with postgre_db as cur:
-    #     init_postgreSQLDatabase(cur)
+    with postgre_db as cur:
+        init_postgreSQLDatabase(cur)
+    with postgre_db as db_cur:
+        data = db_cur.execute("""SELECT GENDER, PRICE, AMOUNT, DATE_ FROM temp_data""").fetchall()
     with ch_client as client_cur:
-        connect_to_clickhouse(client_cur)
-    c = clickhouse_connect.get_client(username="admin", password="admin")
-
-
+        connect_to_clickhouse(client_cur, data)
